3a98fe70ca8b743038e4036df0850bd0a366d8d1
LuddeW <ludwig.wiktorsson@gmail.com>
2015-10-20 17:26:27 +0200

    Start to make a startmenu.

--- PacMan/PacMan/PacManGame.cs
+++ PacMan/PacMan/PacManGame.cs
@@ -4,13 +4,14 @@ using Microsoft.Xna.Framework.Input;
 
 namespace PacMan
 {
-    /// <summary>
-    /// This is the main type for your game.
-    /// </summary>
+    
     public class PacManGame : Game
     {
         GraphicsDeviceManager graphics;
         SpriteBatch spriteBatch;
+        
+        public enum GameState { StartMenu}
+        GameState CurrentState = GameState.StartMenu;
 
         public PacManGame()
         {
@@ -18,64 +19,57 @@ namespace PacMan
             Content.RootDirectory = "Content";
         }
 
-        /// <summary>
-        /// Allows the game to perform any initialization it needs to before starting to run.
-        /// This is where it can query for any required services and load any non-graphic
-        /// related content.  Calling base.Initialize will enumerate through any components
-        /// and initialize them as well.
-        /// </summary>
+        
         protected override void Initialize()
         {
-            // TODO: Add your initialization logic here
+            
 
             base.Initialize();
         }
 
-        /// <summary>
-        /// LoadContent will be called once per game and is the place to load
-        /// all of your content.
-        /// </summary>
+        
         protected override void LoadContent()
         {
-            // Create a new SpriteBatch, which can be used to draw textures.
+            
             spriteBatch = new SpriteBatch(GraphicsDevice);
 
-            // TODO: use this.Content to load your game content here
+            
         }
 
-        /// <summary>
-        /// UnloadContent will be called once per game and is the place to unload
-        /// game-specific content.
-        /// </summary>
+        
         protected override void UnloadContent()
         {
-            // TODO: Unload any non ContentManager content here
+            
         }
 
-        /// <summary>
-        /// Allows the game to run logic such as updating the world,
-        /// checking for collisions, gathering input, and playing audio.
-        /// </summary>
-        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        
         protected override void Update(GameTime gameTime)
         {
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
                 Exit();
+            switch (CurrentState)
+            {
+                case GameState.StartMenu:
+                break;
+            }
 
-            // TODO: Add your update logic here
+            
 
             base.Update(gameTime);
         }
 
-        /// <summary>
-        /// This is called when the game should draw itself.
-        /// </summary>
-        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        
         protected override void Draw(GameTime gameTime)
         {
             GraphicsDevice.Clear(Color.CornflowerBlue);
+            spriteBatch.Begin();
+            switch (CurrentState)
+            {
+                case GameState.StartMenu:
+                    break;
+            }
+            spriteBatch.End();
 
-            // TODO: Add your drawing code here
 
             base.Draw(gameTime);
         }
