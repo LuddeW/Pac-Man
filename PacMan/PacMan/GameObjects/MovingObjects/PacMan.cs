ff367f63e0e79972de85ded55fc5b57c2265e756
LuddeW <ludwig.wiktorsson@gmail.com>
2015-10-27 17:10:11 +0100

    Started with the PacManClass

--- PacMan/PacMan/GameObjects/MovingObjects/PacMan.cs
+++ PacMan/PacMan/GameObjects/MovingObjects/PacMan.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Xna.Framework.Graphics;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -7,5 +8,21 @@ namespace PacMan.GameObjects.MovingObjects
 {
     class PacMan
     {
+        Texture2D pacSprite;
+
+        public PacMan()
+        {
+
+        }
+        public void LoadPictures()
+        {
+
+        }
+
+        // Override?
+        public void Update()
+        {
+
+        }
     }
 }
