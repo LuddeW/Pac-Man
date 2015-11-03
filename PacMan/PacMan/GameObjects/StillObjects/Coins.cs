003513af9c6153d03ced34a0253cea3e255fc7b2
LuddeW <ludwig.wiktorsson@gmail.com>
2015-11-03 21:00:26 +0100

    Prepaird for the coin system

--- PacMan/PacMan/GameObjects/StillObjects/Coins.cs
+++ PacMan/PacMan/GameObjects/StillObjects/Coins.cs
@@ -10,13 +10,19 @@ namespace PacMan.GameObjects.StillObjects
     class Coins : GameObject
     {
         Rectangle posRect;
+
+        public bool visible = true;
+
         public Coins(Texture2D texture, Vector2 pos) : base(texture, pos)
         {
             posRect = new Rectangle((int)pos.X, (int)pos.Y, texture.Width, texture.Height);
         }     
         public void DrawCoin(SpriteBatch spritebatch)
         {
-            spritebatch.Draw(texture, posRect, Color.White);
+            if (visible)
+            {
+                spritebatch.Draw(texture, posRect, Color.White);
+            }           
         }
     }
 }
